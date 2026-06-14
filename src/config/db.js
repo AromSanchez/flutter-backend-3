3004c55ef4d4a5c11034eb17b3ce2bf40d98ddd9
Arom Sanchez <arom.sanchez@tecsup.edu.pe>
2026-06-13 22:58:23 -0500

    remove .env file and unused pool configuration from Sequelize setup

--- src/config/db.js
+++ src/config/db.js
@@ -9,13 +9,7 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
       rejectUnauthorized: false
     }
   },
-  logging: false,
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000
-  }
+  logging: false
 });
 
 module.exports = sequelize;
